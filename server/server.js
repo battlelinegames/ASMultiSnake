04701d1587a1dae142a8f45fd83025d5deae816a
JairusSW <jairus.v.tanaka@outlook.com>
2021-04-02 10:52:40 -0700

    Add Localtunnel 🚀

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 // Sets up an indirect peer-2-peer network.
-// Usage like: socket.send('TO-USER::FROM-USER::MESSAGE')
-// Will send the message to the specified user
+
+// To register: socket.send(':addclient:::NAME-HERE')
+// ^ will register you with the server
+
+// To send message to user: socket.send('TO-USER::FROM-USER::MESSAGE')
+// ^ will send message to FROM-USER
+
+// Server avaliable publicly at: wss://multisnakegame.loca.lt
+
+const localtunnel = require('localtunnel')
 
 const WebSocket = require('ws')
 
@@ -10,12 +18,24 @@ const server = new WebSocket.Server({
 
 const clients = new Map()
 
-server.on('connection', (client, req) => {
+server.on('listening', async () => {
 
-    // Add Client
-    if (!req.headers.name) return
-    
-    clients.set(req.headers.name, client)
+    console.log('Server Listening.')
+
+    console.log('Setting up Peer2Peer')
+
+    console.log('Tunneling with LocalTunnel.me')
+
+    const tunnel = await localtunnel({
+        port: 3000,
+        subdomain: 'multisnakegame'
+    })
+
+    console.log('Server Listening On: ', `${tunnel.url.replace('https://', 'wss://')}`)
+
+})
+
+server.on('connection', (client, req) => {
 
     client.on('message', (data) => {
 
@@ -30,6 +50,11 @@ server.on('connection', (client, req) => {
         const fromUser = context[1]
 
         const content = context[2]
+
+        // Add client (If provided)
+    
+        if (toUser === ':addclient:') return clients.set(fromUser, client)
+        // Signup message would be: ':addclient:::NAME-HERE'
 
         // Reject Unauthorized/Invalid Users
         if (!clients.has(toUser) || !clients.has(fromUser)) return
